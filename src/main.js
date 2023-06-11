1392bfbcc68f9e09c625743c1c4b5005d93b937d
nickbreen <nick@breen.kiwi>
2023-06-11 14:52:23 +1200

    tweak DIY Locale.getWeekInfo polyfill to use Local.weekInfo if available

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const locale = new Intl.Locale(window.navigator.language, {calendar: "gregory"})
 // Polyfill for Firefox https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/getWeekInfo#browser_compatibility
 if (!Intl.Locale.prototype.getWeekInfo)
 {
-    Intl.Locale.prototype.getWeekInfo = ()=> ({firstDay: 7, weekend: [6, 7], minimalDays: 1})
+    Intl.Locale.prototype.getWeekInfo = ()=> (locale.weekInfo || {firstDay: 7, weekend: [6, 7], minimalDays: 1})
 }
 
 const routes = [
